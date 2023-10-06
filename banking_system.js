1021998ef89c6288ecc572942c9a96aa025858f1
fbpr <71777049+fbpr@users.noreply.github.com>
2023-10-06 22:44:08 +0700

    updating banking_system.js

--- banking_system.js
+++ banking_system.js
@@ -1,66 +1,81 @@
 // Febry Prasetya FGA X BINAR BE 1
 class BankAccount {
+  #saldo;
   constructor(saldo) {
+    // Abstraction
     if (this.constructor === BankAccount) {
-      throw new Error('Cannot access this class');
+      throw new Error('Cannot direct access to this class');
     }
-    this.saldo = saldo;
+    this.#saldo = saldo;
   }
 
-  deposit = (nominal) => {
+  // Encapsulation
+  #deposit(nominal) {
     return new Promise((resolve, reject) => {
       if (isNaN(nominal) || nominal <= 0) {
-        reject('Masukkan format nominal yang benar');
+        reject(new Error('Masukkan format nominal yang benar'));
       } else {
         setTimeout(() => {
-          this.saldo += nominal;
-          resolve(this.saldo);
+          this.#saldo += nominal;
+          resolve(this.#saldo);
         }, 2000);
       }
     });
-  };
+  }
 
-  withdraw = (nominal) => {
+  // Encapsulation
+  #withdraw(nominal) {
     return new Promise((resolve, reject) => {
       if (isNaN(nominal) || nominal <= 0) {
-        reject('Masukkan format nominal yang benar');
-      } else if (nominal > this.saldo) {
-        reject('Saldo tidak mencukupi');
+        reject(new Error('Masukkan format nominal yang benar'));
+      } else if (nominal > this.#saldo) {
+        reject(new Error('Saldo tidak mencukupi'));
       } else {
         setTimeout(() => {
-          this.saldo -= nominal;
-          resolve(this.saldo);
+          this.#saldo -= nominal;
+          resolve(this.#saldo);
         }, 2000);
       }
     });
-  };
+  }
+
+  depositSaldo(nominal) {
+    return this.#deposit(nominal);
+  }
+
+  withdrawSaldo(nominal) {
+    return this.#withdraw(nominal);
+  }
+
+  getSaldo() {
+    return this.#saldo;
+  }
 }
 
+//Inheritance
 class BankingSystem extends BankAccount {
   constructor(saldo = 0) {
     super(saldo);
   }
 
-  async deposit(nominal) {
-    try {
-      await super.deposit(nominal);
-    } catch (error) {
-      return error;
-    }
+  depositSaldo(nominal) {
+    return super.depositSaldo(nominal);
   }
 
-  async withdraw(nominal) {
-    try {
-      await super.withdraw(nominal);
-    } catch (error) {
-      return error;
-    }
+  withdrawSaldo(nominal) {
+    return super.withdrawSaldo(nominal);
+  }
+
+  saldo() {
+    return super.getSaldo();
   }
 }
 
+// Object Instantiation
 const account = new BankingSystem();
-document.getElementById('saldo').value = account.saldo;
+document.getElementById('saldo').value = account.saldo();
 
+// DOM
 const depositButton = document.getElementById('deposit');
 depositButton.addEventListener('click', async () => {
   try {
@@ -69,17 +84,17 @@ depositButton.addEventListener('click', async () => {
     );
 
     if (!nominal) {
-      throw 'Deposit dibatalkan';
+      throw new Error('Deposit dibatalkan');
     }
 
     document.getElementById('status').textContent = 'Processing...';
 
-    const deposit = await account.deposit(Number(nominal));
+    const deposit = await account.depositSaldo(Number(nominal));
 
     document.getElementById('status').textContent = 'Deposit Berhasil';
     document.getElementById('saldo').value = deposit;
   } catch (error) {
-    document.getElementById('status').textContent = error;
+    document.getElementById('status').textContent = error.message;
   }
 });
 
@@ -89,16 +104,16 @@ withdrawButton.addEventListener('click', async () => {
     const nominal = window.prompt('Masukkan jumlah saldo yang ingin dikurangi');
 
     if (!nominal) {
-      throw 'Withdraw dibatalkan';
+      throw new Error('Withdraw dibatalkan');
     }
 
     document.getElementById('status').textContent = 'Processing...';
 
-    const withdraw = await account.withdraw(Number(nominal));
+    const withdraw = await account.withdrawSaldo(Number(nominal));
 
     document.getElementById('status').textContent = 'Withdraw Berhasil';
     document.getElementById('saldo').value = withdraw;
   } catch (error) {
-    document.getElementById('status').textContent = error;
+    document.getElementById('status').textContent = error.message;
   }
 });
